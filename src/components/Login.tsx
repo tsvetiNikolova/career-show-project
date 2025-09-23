8ac25d9b44e1f15f411d7bbb95d19eab5fbce3c2
tsvetiNikolova <tsveti.nikolova@diadraw.com>
2025-09-23 11:30:22 +0300

    updated login screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import careerExplorerLogo from '../assets/Career_game_logo.svg';
+import careerShowLogo from '../assets/careerShowLogo.svg';
 import diadrawLogo from '../assets/diadrawLogo.svg';
 import useLoginViewmodel from '../viewModels/useLoginViewmodel';
 
@@ -14,13 +14,14 @@ function LandingPage({ page, setPage, setUserName }: LandingPageProps) {
 
     return (
         <div>
-            <img src={careerExplorerLogo} alt='career explorer logo' />
+            <img src={careerShowLogo} alt='career explorer logo' style={{margin:'2.5em 0 0.8em 0'}} />
+            <h5 style={{margin:0, fontWeight:800}}>X</h5>
+            <img src={diadrawLogo} style={{ width: '40vw', margin:'0.8em 0 2.5em 0' }} alt='diadraw logo' />
             <h5 style={{ width: '50vw', margin: 'auto' }}>Meet top 32 employers and explore career opportunities, all in one place.</h5>
             <p>Get a chance to win a prize!</p>
-            <p>Powered by</p>
-            <img src={diadrawLogo} style={{ width: '40vw' }} alt='diadraw logo' />
+           
             {/* implement it */}
-            <h3 style={{ fontWeight: 400, margin: '1.5em 0' }}>36 Active Now</h3>
+            <h3 style={{ fontWeight: 400, margin: '1.5em 0' }}>36 Players Exploring</h3>
             <form onSubmit={(e) => e.preventDefault()}>
                 <label className='labelForInput' htmlFor='name'>Full name</label>
                 <input
@@ -63,13 +64,13 @@ function LandingPage({ page, setPage, setUserName }: LandingPageProps) {
                         </p>
                     </label>
                 </div>
+            <button onClick={handleStartExploring} style={{width: '80%', marginBottom: '3em'}}>Start exploring</button>
             </form>
             <p className='textGray'>
                 Your data will not be stored or processed for purposes other than this game.
                 For any questions regarding your data, please contact <u style={{ color: 'black' }}>info@diadraw.com</u>
             </p>
 
-            <button onClick={handleStartExploring}>Start exploring</button>
 
 
 
